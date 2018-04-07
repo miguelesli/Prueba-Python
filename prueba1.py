a6202781644844825e15a767096af8716cbb63f2
miguelesli <38160527+miguelesli@users.noreply.github.com>
2018-04-07 13:34:09 -0500

    avance en clases
    corregir comentarios

---
+++ prueba1.py
@@ -0,0 +1,84 @@
+"""nombre = "Esli"
+edad = 31
+print ("hola", nombre,"\ntu edad es:",edad)
+#segunda parte 
+a= b=3
+c = "Esli"
+print (a, b, c)
+"""#segunda parte
+"""print ("cual es tu nombre")
+nombre2 =input()"""
+"""print ("hola como estas ",nombre2, " ?")"""
+"""comentario
+en
+multiples comillas """
+"""# operadores
+
+nombre3="Esli ALvarez"
+edad =31
+Facultad = "aragon"
+Estatura = 1.85
+sangre = "B+"
+carrera ="ico"
+Nacionalidad ="Mexicano"
+print ("nombre", nombre3,"\n tu edad es ", edad, "\n estudiaste en la facultad ",
+       Facultad, "\nTu estatura es ", Estatura, "\nTipo de sangre: ",
+       sangre, "\n tu carrera es: ", carrera, "\nY eres :",Nacionalidad)"""
+"""
+#input
+print ("ingresa tu nombre")
+nombre3=input()
+print ("\nIngresa tu edad")
+edad = input()
+print ("\nIngresa tu Facultad de estudio: ")
+Facultad = input()
+print ("\n Ingresa tu estatura")
+Estatura = input()
+print("\nIngresa tu tipo de sangre")
+sangre = input()
+print("\nQue carrera cursaste? ")
+carrera =input()
+print("\nY por ultimo cual es tu nacionalidad")
+Nacionalidad =input()
+print ("nombre", nombre3,"\n tu edad es ", edad, "\n estudiaste en la facultad ",
+       Facultad, "\nTu estatura es ", Estatura, "\nTipo de sangre: ",
+       sangre, "\n tu carrera es: ", carrera, "\nY eres :",Nacionalidad)"""
+
+"""#input interno erroneo
+print ("ingresa tu nombre")
+nombre3=input("esli")
+print ("\nIngresa tu edad")
+edad = input("31")
+print ("\nIngresa tu Facultad de estudio: ")
+Facultad = input("Aragon")
+print ("\n Ingresa tu estatura")
+Estatura = input("1.85")
+print("\nIngresa tu tipo de sangre")
+sangre = input("b+")
+print("\nQue carrera cursaste? ")
+carrera =input("ICO")
+print("\nY por ultimo cual es tu nacionalidad")
+Nacionalidad =input("mexicano")
+print ("nombre", nombre3,"\n tu edad es ", edad, "\n estudiaste en la facultad ",
+       Facultad, "\nTu estatura es ", Estatura, "\nTipo de sangre: ",
+       sangre, "\n tu carrera es: ", carrera, "\nY eres :",Nacionalidad)"""
+#otro input
+
+
+print ("ingresa tu nombre")
+input(nombre3)
+print ("\nIngresa tu edad")
+input(edad)
+print ("\nIngresa tu Facultad de estudio: ")
+input(Facultad)
+print ("\n Ingresa tu estatura")
+input(Estatura)
+print("\nIngresa tu tipo de sangre")
+input(sangre)
+print("\nQue carrera cursaste? ")
+input(carrera)
+print("\nY por ultimo cual es tu nacionalidad")
+input(Nacionalidad)
+print ("nombre", nombre3,"\n tu edad es ", edad, "\n estudiaste en la facultad ",
+       Facultad, "\nTu estatura es ", Estatura, "\nTipo de sangre: ",
+       sangre, "\n tu carrera es: ", carrera, "\nY eres :",Nacionalidad)
