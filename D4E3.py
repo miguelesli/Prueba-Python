2a9ccafbeb85727f2c07d901d986bf2fe45a61e1
miguelesli <38160527+miguelesli@users.noreply.github.com>
2018-04-28 12:48:20 -0500

    ejemplo

---
+++ D4E3.py
@@ -0,0 +1,5 @@
+numero =int(input("escriva un numero positivo"))
+while numero <0:
+  print("ha escrito un numero negativo, intende te de nuevo")
+  numero =int(input("escriva un numero positivo"))
+print ("gracias por su coperacion")
